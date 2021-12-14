b1dd30a2bc009ea7b9e0fccc58197104d4b3ad13
usr1488 <justzone95@gmail.com>
2021-12-14 22:21:16 +0200

    init

---
+++ src/main/java/nig/ger/controller/MainController.java
@@ -0,0 +1,12 @@
+package nig.ger.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/main")
+    public String main() {
+        return "main";
+    }
+}
